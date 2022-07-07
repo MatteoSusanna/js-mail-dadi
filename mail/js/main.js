4f924d05cd6f6eef8688606d525ee4042f3a48b9
Matteo Susanna <matteo2784@gmail.com>
2022-07-07 17:05:37 +0200

    Fine esercizio

--- mail/js/main.js
+++ mail/js/main.js
@@ -9,15 +9,17 @@ stampa un messaggio appropriato sull’esito del controllo.
 const mail = ['matteo@gmail.com', 'roberta@gmail.com', 'federico@gmail.com', 'carla@gmail.com'];
 const mailIns = prompt('inserisci la tua email');
 
-
+let ric;
 
 for (let i = 0; i < mail.length; i++) {
     
     if(mail[i] == mailIns){
+        ric = 'si';
         alert ('la mail ' + mailIns + ' è nella lista');
-        i = 4;
     }
     
 }
 
-
+if(ric != 'si'){
+    alert ('la mail ' + mailIns + ' non è nella lista');
+}
